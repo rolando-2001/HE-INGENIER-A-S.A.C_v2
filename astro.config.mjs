8b9132e4eff5332b47665aa2f6c5999225e268f5
rolando-2001 <rolando.casapaico@tecsup.edu.pe>
2025-03-14 19:18:46 -0500

    Add initial project setup with assets, environment configuration, and core components

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig, envField } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  env: {
+    schema: {
+      MAILER_SERVICE: envField.string({ context: "server", access: "secret" }),
+      MAILER_EMAIL: envField.string({ context: "server", access: "secret" }),
+      MAILER_SECRET_KEY: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
+
+  output: 'server',
+  adapter: vercel({}),
+});
